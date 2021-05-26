b3e04206082187843c568338d2972c3eb61bd6cd
aojunjie <aojunjie@yuanzhi.com>
2021-05-26 11:28:59 +0800

    update(pages): 1. 新增taroUI

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,7 +1,20 @@
 export default {
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/taroUI/index'
   ],
+  tabBar: {
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        text: '首页'
+      },
+      {
+        pagePath: 'pages/taroUI/index',
+        text: 'Taro UI'
+      }
+    ]
+  },
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
